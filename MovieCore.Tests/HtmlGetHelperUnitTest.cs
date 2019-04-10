b8813cc87209e1d192082bd0c110c49596853d96
Desktop-ZHOU <yatong.zhou@gmail.com>
2019-04-10 22:29:25 +1000

    Fix test, add async

--- MovieCore.Tests/HtmlGetHelperUnitTest.cs
+++ MovieCore.Tests/HtmlGetHelperUnitTest.cs
@@ -8,26 +8,26 @@ namespace MovieCore.Tests
         [Theory]
         [InlineData("cinemaworld")]
         [InlineData("filmworld")]
-        public void HtmlGetHelper_Should_GetMovieData(string databaseName)
+        public async void HtmlGetHelper_Should_GetMovieData(string databaseName)
         {
             string WebsiteDomain = "http://webjetapitest.azurewebsites.net";
             string url = $"{WebsiteDomain}/api/{databaseName}/movies";
 
-            var filmResult = HtmlGetHelper.GetResult(url);
+            var filmResult = await HtmlGetHelper.GetResult(url);
 
             Assert.NotNull(filmResult);
         }
 
         [Fact]
-        public void HtmlGetHelperWithInvalidDatabaseName_Should_ReturnNull()
+        public async void HtmlGetHelperWithInvalidDatabaseName_Should_ReturnNull()
         {
             string WebsiteDomain = "http://webjetapitest.azurewebsites.net";
             string databaseName = "invalidMovieDatabase";
             string url = $"{WebsiteDomain}/api/{databaseName}/movies";
 
-            var filmResult = HtmlGetHelper.GetResult(url);
+            var filmResult = await HtmlGetHelper.GetResult(url);
 
-            Assert.Null(filmResult.Result);
+            Assert.Null(filmResult);
         }
     }
 }
